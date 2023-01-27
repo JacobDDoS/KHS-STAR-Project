2775ddf310ed51c15e206895eb3d18105ea0da06
JacobDDoS <jacdimmitt@gmail.com>
2023-01-27 17:44:31 -0600

    Fixed Form Date bug

--- star-frontend/src/Components/Admin/IndividualData.js
+++ star-frontend/src/Components/Admin/IndividualData.js
@@ -5,10 +5,13 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { useState } from 'react';
+import { allDataComponentsExamples } from '../../data/allDataComponents';
+import { componentNameToIndex } from '../../helpers/componentNameToIndex';
 
 export const IndividualData = ({data, setData, component}) => {
     const copyOfData = JSON.parse(JSON.stringify(data));
-    console.log(data, " ", component);
+    const exampleData = allDataComponentsExamples[componentNameToIndex(component)];
+    console.log(exampleData);
 
     const handleChange = (e) => {
         copyOfData[e.target.id] = e.target.value;
@@ -18,11 +21,11 @@ export const IndividualData = ({data, setData, component}) => {
     return (
     <div>
         {
-            Object.entries(copyOfData).map((entry)=>{
+            Object.entries(copyOfData).map((entry, idx)=>{
                 if (entry[0] === "id") return;
                 return <div>
                     <label>{entry[0]}</label>
-                    {Date.parse(entry[1]) && entry[1].includes(':') ? 
+                    {Date.parse(exampleData[entry[0]]) && exampleData[entry[0]].includes(':') ? 
                     <>
                     <br/>
                     <br/>
