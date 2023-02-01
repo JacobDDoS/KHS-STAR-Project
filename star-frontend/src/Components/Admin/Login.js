bdacee2a65ef4f18854fac58b415afaae4856d61
JacobDDoS <jacdimmitt@gmail.com>
2023-01-31 22:13:12 -0600

    Styled login and created redirect from login

--- star-frontend/src/Components/Admin/Login.js
+++ star-frontend/src/Components/Admin/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import cookies from '../../data/cookies'
 
 import serverURL from '../../data/serverURL';
@@ -7,6 +8,8 @@ import serverURL from '../../data/serverURL';
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -17,49 +20,9 @@ const Login = () => {
     setPassword(e.target.value);
   }
 
-  const seeIfLoggedIn = async () => {
-
-    const opts = {  method: "GET", 
-                    'credentials': 'include',
-                    headers: new Headers({
-                        'Accept': 'application/json',
-                        'Access-Control-Allow-Origin':'http://localhost:3000/',
-                        'Content-Type': 'application/json',
-                    })
-                 }
-
-    fetch(`${serverURL}/homepage/pictureShowcase`, opts)
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
-
-  const addAward = async () => {
-    console.log("called");
-    const delOptions = {
-        credentials: 'include',
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Access-Control-Allow-Origin':'http://localhost:3000/',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "membersOfAward": "Adan;Alex;Jacob",
-            "date": "2022-11-26T18:52:18.366+00:00",
-            "awardName": "Best STAR Project",
-            "pictureOfAwardURL": "htsfdfstps://cdn.mos.cms.futurecdn.net/LZUsfgcXH7WMeVvuHYsoSA-1200-80.png",
-            "description": "Thfsfsdfsdfese dudes were so awesome with their STsdfsdAR project omg!"
-        })
-    }
-
-    fetch(`${serverURL}/api/add/Award`, delOptions)
-        .then(res => console.log(res))
-            // .then(response=>response.json())
-            // .then(data=>console.log(data))
-  }
-
   const handleLogin = (e) => {
     e.preventDefault();
+    
     const data = new FormData();
     data.append('username', username);
     data.append('password', password);
@@ -74,11 +37,16 @@ const Login = () => {
 
     fetch(`${serverURL}/login`, requestOptions)
         .then(response => [...response.headers])
-        .then(data => console.log(data));
+        .then(data => 
+            {
+                if (data.length === 4) {
+                    navigate("/admin");
+                }
+            });
   }
 
   return (
-    <div>
+    <div style={{"height": "100vh", "marginTop": "100px"}}>
         <h2>Login</h2>
         <form onSubmit={handleLogin}>
             <div>
@@ -90,9 +58,13 @@ const Login = () => {
                 <input type="password" value={password} onChange={handlePasswordChange} />
             </div>
             <div>
-                <button>
+                <button type='submit'>
                     Login
                 </button>
+            </div>
+            <br/>
+            <div>
+                <p><b>Note:</b> that if you login correctly, it will redirect you to the admin page where you can create, update, & delete information</p>
             </div>
         </form>
         
